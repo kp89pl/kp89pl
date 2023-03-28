d56d2b1abd3e6662d3f3c6890e25256b4f326586
kp89pl@gmail.com <kp89pl@gmail.com>
2023-03-28 21:49:29 +0200

    zad dom dz 5

--- 1Appka/Program.cs
+++ 1Appka/Program.cs
@@ -1,19 +1,69 @@
-﻿string name = "Ewa";
-char sex = 'K';
-int age = 33;
+﻿int number = 54321;
 
-if (sex == 'K' && age < 18)
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
+
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta poniżej lat 30");
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
-
-else if (age == 33 && sex == 'K')
-
-{
-    Console.WriteLine("Ewa lat 33");
-}
-
-else if  (name == "Ewa" && age < 30)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
+Console.WriteLine("Ilość cyfr w liczbie 54321");
+Console.WriteLine("Cyfr 1 jest" + counter1);
+Console.WriteLine("Cyfr 2 jest" + counter2);
+Console.WriteLine("Cyfr 3 jest" + counter3);
+Console.WriteLine("Cyfr 4 jest" + counter4);
+Console.WriteLine("Cyfr 5 jest" + counter5);
+Console.WriteLine("Cyfr 6 jest" + counter6);
+Console.WriteLine("Cyfr 7 jest" + counter7);
+Console.WriteLine("Cyfr 8 jest" + counter8);
+Console.WriteLine("Cyfr 9 jest" + counter9);
